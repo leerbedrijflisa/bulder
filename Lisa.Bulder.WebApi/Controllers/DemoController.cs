273fc25ec8cd8f8df8896f72fc5620d307ec0c85
ivyk96 <ivyk96@live.com>
2015-09-24 14:49:54 +0200

    Created WebApi.

---
+++ Lisa.Bulder.WebApi/Controllers/DemoController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNet.Mvc;
+
+namespace Lisa.Bulder.WebClient.Controllers
+{
+    [Route("")]
+    public class DemoController : Controller
+    {
+        public IActionResult Get()
+        {
+            var person = new { FirstName = "Mike", LastName = "Faro" };
+            return new ObjectResult(person);
+        }
+    }
+}

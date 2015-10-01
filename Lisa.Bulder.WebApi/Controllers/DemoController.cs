781727bc28c6e32627919df78f84cbaee3ee8198
William Willing <william@williamwilling.com>
2015-10-01 11:12:05 +0200

    The Web API now gets its data from Table Storage.

--- Lisa.Bulder.WebApi/Controllers/DemoController.cs
+++ Lisa.Bulder.WebApi/Controllers/DemoController.cs
@@ -1,18 +1,30 @@
 ﻿using Microsoft.AspNet.Mvc;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+using System.Threading.Tasks;
 
 namespace Lisa.Bulder.WebClient.Controllers
 {
     [Route("")]
     public class DemoController : Controller
     {
-        public IActionResult Get()
+        public async Task<IActionResult> Get()
         {
-            var person = new object[]
-            {
-                new { FirstName = "Mike", LastName = "Faro" },
-                new { FirstName = "Rick", LastName = "Jelier" }
-            };
-            return new ObjectResult(person);
+            var account = CloudStorageAccount.Parse("UseDevelopmentStorage=true");
+            var client = account.CreateCloudTableClient();
+            var table = client.GetTableReference("demo");
+            await table.CreateIfNotExistsAsync();
+
+            var query = new TableQuery<Person>();
+            var segment = await table.ExecuteQuerySegmentedAsync(query, null);
+            
+            return new ObjectResult(segment.Results);
         }
+    }
+
+    public class Person : TableEntity
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
     }
 }

9f8161ce5ef4758a8de31bf5d7d257039caed1f2
Mike Faro <ivyk96@live.com>
2015-10-30 11:01:10 +0100

    Added an overview of channels

--- Lisa.Bulder.WebClient/wwwroot/app.js
+++ Lisa.Bulder.WebClient/wwwroot/app.js
@@ -1,9 +1,10 @@
 ﻿export class App {
     configureRouter(config, router) {
         config.map([
-            { route: "messages/create", moduleId: "messages/create" },
             {route: "users/create", moduleId: "users/create" },
             {route: "channels/create", moduleId: "channels/create" },
+            {route: "channels/overview", moduleId: "channels/overview" },
+            { route: "messages/create", moduleId: "messages/create" },
             { route: "messages/overview", moduleId: "messages/overview"}
         ])
     }

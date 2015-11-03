fce2b885d313a88bc64d5b21142da81be8a9d1db
Tim <timkeulen@gmail.com>
2015-11-03 14:06:31 +0100

    Finished coding to create messages!

--- Lisa.Bulder.WebClient/wwwroot/messages/create.js
+++ Lisa.Bulder.WebClient/wwwroot/messages/create.js
@@ -2,24 +2,32 @@
 
 export class Create {
     activate() {
+
+        var webapi = new HttpClient().configure(config => {
+            config.withBaseUrl("http://localhost:13693");
+
+        });
+
         this.author = "author";
 
         //get all channels
         webapi.get("/channels").then(response => {
-            this.messages = response.content;
+            this.channels = response.content;
         });
     }
     submit() {
         var message = {
             author: this.author,
-            text: this.text
+            text: this.text,
+            partitionKey: this.selectChannel
         };
-
         var webapi = new HttpClient().configure(config => {
             config.withBaseUrl("http://localhost:13693");
+            config.withHeader("Content-Type", "application/json");
         });
-
+        console.log(message);
         webapi.post("/messages", message);
+
     }
 
 }
